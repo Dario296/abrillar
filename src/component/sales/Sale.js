a2abb114946dacaf9686e68406adc048f97a21ce
Dario296 <dariorodriguez296@yahoo.com.ar>
2023-12-15 17:17:30 -0300

    arreglo error

--- src/component/sales/Sale.js
+++ src/component/sales/Sale.js
@@ -98,7 +98,7 @@ const Sale = ({ producto, handleAgregar, recargar }) => {
 			) : (
 				<>
 					<td>{producto.nombre}</td>
-					<td>{producto.stock.toFixed(2)}</td>
+					<td>{producto.stock}</td>
 					<td>{precioU}</td>
 					<td>
 						<input className='inputVentas' onChange={handleChangeCantidad} type='number' name='cantidad' value={cantidad} />
