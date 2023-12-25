eb2e83ae6a2383b8848f1846af69296cd9816970
Dario296 <dariorodriguez296@yahoo.com.ar>
2023-12-25 16:14:49 -0300

    agrego buscador de fechas en ventas

--- src/component/totalForTheDay/TotalForTheDayList.js
+++ src/component/totalForTheDay/TotalForTheDayList.js
@@ -2,12 +2,19 @@ import { collection, getDocs, getFirestore, query, where } from 'firebase/firest
 import React, { useEffect, useState } from 'react';
 import app from '../config/firebase';
 import TotalForTheDay from './TotalForTheDay';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateField } from '@mui/x-date-pickers/DateField';
+import { Button } from "@mui/material";
+import dayjs from 'dayjs';
 
 const db = getFirestore(app);
 
 const TotalForTheDayList = () => {
 	const [ventas, setVentas] = useState([]);
-	const Fecha = new Date().toLocaleDateString();
+	const [Fecha, setFecha] = useState(new Date().toLocaleDateString());
+	const [busqueda, setBusqueda] = useState('');
 
 	useEffect(() => {
 		const ProductosRef = collection(db, 'Ventas');
@@ -21,9 +28,25 @@ const TotalForTheDayList = () => {
 	const total = () => {
 		return ventas.reduce((acc, venta) => acc + parseInt(venta.total), 0);
 	};
+	
+	const buscarFecha = () => {
+		if (busqueda === '') return;
+		let fechaFormateada = dayjs(busqueda).format('DD/MM/YYYY');
+		setFecha(fechaFormateada);
+		setBusqueda('')
+	} 
+
 
 	return (
 		<>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<DemoContainer components={['DateField']}>
+					<form>
+						<DateField format="DD-MM-YYYY" onChange={(newValue) => setBusqueda(newValue)}/>
+						<Button onClick={buscarFecha}>Buscar fecha</Button>
+					</form>
+				</DemoContainer>
+			</LocalizationProvider>
 			<div>
 				<h1 className='text-center'>
 					Total de Ventas del Día: {Fecha} ${total()}
